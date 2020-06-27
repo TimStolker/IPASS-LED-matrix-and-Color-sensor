862144e9db7fb2ae6b494a32976ee155c58323a4
TimStolker <60575792+TimStolker@users.noreply.github.com>
2020-06-27 09:34:41 +0200

    Create sensor.hpp

---
+++ src/LEDmatrix/sensor.hpp
@@ -0,0 +1,50 @@
+
+//this file contains Doxygen lines
+///file sensor.hpp
+/// \brief Datastruct for returning the RGB values.
+/// \details This class needs 5 digital datapins to work correctly.
+
+
+
+
+#ifndef SENSOR_HPP
+#define SENSOR_HPP
+
+#define SAM3XA 1
+
+#define register 
+#include "sam.h"
+#undef register
+#include <cstdint>
+
+
+
+
+class sensor{
+    private:
+    Pio* port_out;
+    uint32_t mask_out;
+    Pio* port_s0;
+    uint32_t mask_s0;
+    Pio* port_s1;
+    uint32_t mask_s1;
+    Pio* port_s2;
+    uint32_t mask_s2;
+    Pio* port_s3;
+    uint32_t mask_s3;
+
+    public:
+    /// \brief Constructor for all the digital data pins.
+    sensor(Pio* port_out, uint32_t mask_out, 
+    Pio* port_s0,uint32_t mask_s0,
+    Pio* port_s1,uint32_t mask_s1,
+    Pio* port_s2,uint32_t mask_s2,
+    Pio* port_s3,uint32_t mask_s3);
+
+    /// \brief This returns a 32 bit value with each 8 bits being a color. From left to right: Red,Green,Blue. The first 8 bits are valued 0.
+    uint32_t getRGB();
+
+
+};
+
+#endif
